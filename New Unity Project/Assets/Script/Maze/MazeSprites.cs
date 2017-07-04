e335d8c67f9c4dc128c581b2715d6e02edfe13c9
Elk0ooo <nicolaslbt@yahoo.com>
2017-07-04 22:58:03 +0200

    salurt
    sdfsdfsd fds fds fds fds

---
+++ New Unity Project/Assets/Script/Maze/MazeSprites.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeSprites : MonoBehaviour {
+    SpriteRenderer sRenderer;
+    public void SetSprite(Sprite sprite, int sortingOrder)
+    {
+        sRenderer.sprite = sprite;
+        sRenderer.sortingOrder = sortingOrder;
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        SetSprite(sprite, 0);
+    }
+    void Awake()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Script/Tools/Tools.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour {
+    public static T[] Shuffle<T>(T[] array, System.Random rg)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int randomIndex = rg.Next(i, array.Length);
+
+            T tempItem = array[randomIndex];
+
+            array[randomIndex] = array[i];
+            array[i] = tempItem;
+        }
+
+        return array;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
