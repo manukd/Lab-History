177a9d555ef2111b527bd1ce429116ee6844c4f9
Elk0ooo <nicolaslbt@yahoo.com>
2017-07-04 23:05:12 +0200

    Wesh alors

---
+++ New Unity Project/Assets/Script/Maze/MazeDirective.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeDirective : MonoBehaviour {
+    public int keysToFind;
+    public Text keysValueText;
+    public MazeGoal mazeGoalPrefab;
+    public MazeKey mazeKeyPrefab;
+  
+    MazeGoal mazeGoal;
+    int foundKeys;
+    List<Vector3> mazeKeyPositions;
+
+    public delegate void MazeReadyAction();
+    public static event MazeReadyAction OnMazeReady;
+    // Use this for initialization
+    void Start () {
+        if (OnMazeReady != null)
+        {
+            OnMazeReady();
+        }
+    }
+    void Awake()
+    {
+        MazeGenerator.OnMazeReady += StartDirectives;
+    }
+
+    void StartDirectives()
+    {
+        mazeGoal = Instantiate(mazeGoalPrefab, MazeGenerator.instance.mazeGoalPosition, Quaternion.identity) as MazeGoal;
+        mazeGoal.transform.SetParent(transform);
+
+        mazeKeyPositions = MazeGenerator.instance.GetRandomFloorPositions(keysToFind);
+
+        for (int i = 0; i < mazeKeyPositions.Count; i++)
+        {
+            MazeKey mazeKey = Instantiate(mazeKeyPrefab, mazeKeyPositions[i], Quaternion.identity) as MazeKey;
+            mazeKey.transform.SetParent(transform);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Script/Maze/MazeGoal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGoal : MonoBehaviour {
+    public Sprite closedGoalSprite;
+    public Sprite openedGoalSprite;
+    // Use this for initialization
+    void Start () {
+        GetComponentInChildren<SpriteRenderer>().sprite = closedGoalSprite;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OpenGoal()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = openedGoalSprite;
+    }
+  
+}
